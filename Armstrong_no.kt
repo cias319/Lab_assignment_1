3dd175e8b10ebaf38516871c556088d455c5890a
Chiranjib Swain <chiranjibswain2003@gmail.com>
2024-06-28 13:26:38 +0530

    Create Armstrong_no.kt

---
+++ Armstrong_no.kt
@@ -0,0 +1,53 @@
+import java.util.*
+
+fun main() {
+
+    val read = Scanner(System.`in`)
+
+    val number = read.nextInt()
+
+    var temp = number
+
+    var numDigits = 0
+
+    var sum = 0
+
+
+
+    while (temp != 0) {
+
+        numDigits++
+
+        temp /= 10
+
+    }
+
+
+
+    temp = number
+
+
+
+    while (temp != 0) {
+
+        val digit = temp % 10
+
+        sum += Math.pow(digit.toDouble(), numDigits.toDouble()).toInt()
+
+        temp /= 10
+
+    }
+
+
+
+    if (sum == number) {
+
+        println("$number is an Armstrong number.")
+
+    } else {
+
+        println("$number is not an Armstrong number.")
+
+    }
+
+}
